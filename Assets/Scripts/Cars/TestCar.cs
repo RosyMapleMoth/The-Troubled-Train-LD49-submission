f33e665853a908e28ff21051605e361990f5f697
Eli Peters <epeters589@gmail.com>
2021-10-02 15:23:21 -0700

    Test Car game
    The test car will now activate the coal game when it's revealed.

---
+++ Assets/Scripts/Cars/TestCar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCar : MonoBehaviour
+{
+    public Control coal; // The prefab for the coal minigame
+
+    private ControlController controller;
+
+    void Awake()
+    {
+        controller = (ControlController)FindObjectOfType<ControlController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller.addSpecificControl(coal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
