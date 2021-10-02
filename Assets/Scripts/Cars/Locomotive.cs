3fd1a0f9e1115be285403c1089fafe7af0acb19b
Eli Peters <epeters589@gmail.com>
2021-10-02 14:45:50 -0700

    Coal minigame
    The game now starts with the coal minigame.  Also some rearranging and fixed a typo

---
+++ Assets/Scripts/Cars/Locomotive.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotive : MonoBehaviour
+{
+    public Control coal; // The prefab for the coal minigame
+
+    private ControlController controller;
+
+    void Awake()
+    {
+        controller = (ControlController)FindObjectOfType<ControlController>();
+    }
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller.addSpecificControl(coal);
+        //controller.addRandomControl();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
