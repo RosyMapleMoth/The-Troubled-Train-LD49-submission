029f7bda323d63ca03dd22a55025f83415d1a81f
Eli Peters <epeters589@gmail.com>
2021-10-02 20:20:39 -0700

    Lazer Car
    Added A Lazer Car with a rotating gun that matches its game.

---
+++ Assets/Scripts/Cars/LazerCar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerCar : MonoBehaviour
+{
+    public Control gamePrefab;  // Assign your minigame prefab to this in the car's prefab
+
+    public GameObject gun;
+
+    private ControlController controller;
+
+    private Lazer myGame;
+
+    void Awake()
+    {
+        controller = FindObjectOfType<ControlController>();
+    }
+
+    void Start()
+    {
+        controller.addSpecificControl(gamePrefab);
+    }
+
+    void Update()
+    {
+        gun.transform.rotation = Quaternion.Euler(-45f, ((myGame.GetLeftOrRight() - 0.5f) * -90f), 0f);
+    }
+
+    public bool HasGame()
+    {
+        return (myGame != null);
+    }
+
+    public void SetGame(Lazer game)
+    {
+        myGame = game;
+
+
+    }
+}
