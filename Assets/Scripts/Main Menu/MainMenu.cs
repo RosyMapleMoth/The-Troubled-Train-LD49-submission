c7ba54c1ab643376df45022c85342b7e1be7af03
Eli Peters <epeters589@gmail.com>
2021-10-03 19:08:48 -0700

    Main Menu
    Made a main menu

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject creditsPanel;
+
+    public void PlayGame()
+    {
+
+    }
+
+    public void CreditsToggle()
+    {
+        if(creditsPanel.activeSelf)
+        {
+            creditsPanel.SetActive(false);
+        }
+        else
+        {
+            creditsPanel.SetActive(true);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
