a24551d112e7aae68cd87683d4700dea5333333d
Eli Peters <epeters589@gmail.com>
2021-10-03 20:40:10 -0700

    Game UI
    Added UI to the game!

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public GroundCycler groundCycler;
+
+    public Text speedText;
+    public Text distanceText;
+
+    public GameObject pauseScreen;
+
+    private float distance = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseToggle();
+        }
+
+        distance += (groundCycler.speed * Time.deltaTime);
+
+        speedText.text = ("Speed: " + KPHConvert(groundCycler.speed).ToString("F2") + " KPH");
+        distanceText.text = ("Distance: " + distance.ToString("F2") + " M");
+    }
+
+    public float KPHConvert(float input)
+    {
+        return (input * 3.6f);
+    }
+
+    public void PauseToggle()
+    {
+        if(pauseScreen.activeSelf)
+        {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
